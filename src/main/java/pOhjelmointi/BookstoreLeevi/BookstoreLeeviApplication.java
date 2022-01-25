9641662fef9dd0a5fbdde650e34b573b5f7d2066
unknown <leeviva@gmail.com>
2022-01-25 20:25:40 +0200

    harjoituksen alustus

---
+++ src/main/java/pOhjelmointi/BookstoreLeevi/BookstoreLeeviApplication.java
@@ -0,0 +1,13 @@
+package pOhjelmointi.BookstoreLeevi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreLeeviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreLeeviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pOhjelmointi/BookstoreLeevi/BookstoreLeeviApplicationTests.java
@@ -0,0 +1,13 @@
+package pOhjelmointi.BookstoreLeevi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreLeeviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
